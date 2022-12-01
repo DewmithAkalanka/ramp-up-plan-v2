70e88fc5134aa0cf6c1408e675258a44e0b805b7
Dewmith Akalanka <dewmitha@fortude.co>
2022-12-01 15:12:10 +0530

    Add edit student fucntionality

--- client/src/app/edit-student/edit-student.component.ts
+++ client/src/app/edit-student/edit-student.component.ts
@@ -2,6 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Apollo, gql } from 'apollo-angular';
+
+const EDIT_STUDENT = gql`
+  mutation ($id: Int!, $name: String!, $email: String!, $dob: DateTime!) {
+    updateStudent(
+      updateStudentInput: { id: $id, name: $name, email: $email, dob: $dob }
+    ) {
+      id
+    }
+  }
+`;
 
 @Component({
   selector: 'app-edit-student',
@@ -9,7 +20,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./edit-student.component.css'],
 })
 export class EditStudentComponent implements OnInit {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private apollo: Apollo
+  ) {}
 
   id = this.data.id;
   name = this.data.name;
@@ -27,7 +41,27 @@ export class EditStudentComponent implements OnInit {
     });
   }
 
-  onSubmit() {
-    console.log('Submitted');
+  onEdit() {
+    this.apollo
+      .mutate({
+        mutation: EDIT_STUDENT,
+        variables: {
+          id: this.id,
+          name: this.reactiveForm.value.name,
+          email: this.reactiveForm.value.email,
+          dob: this.reactiveForm.value.dob,
+        },
+      })
+      .subscribe(
+        (result: any) => {
+          console.log(result);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+  onDelete() {
+    console.log('Deleted');
   }
 }
